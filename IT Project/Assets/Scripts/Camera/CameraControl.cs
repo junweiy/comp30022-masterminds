665962f38f7679121b183b7d1c2bcadb443e8e17
DESKTOP-9H4UB0A\Kevin <weixuc@student.unimelb.edu.au>
2016-08-24 13:19:09 +1000

    - added navigation - added health bar - added simple UI to display gold

---
+++ IT Project/Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+    
+    // The target that the camera will follow
+    public Transform m_Target; 
+
+
+    private void Awake()
+    {
+        this.transform.position = m_Target.position;
+    }
+
+    // Fixed update will sync the position
+    private void FixedUpdate()
+    {
+
+        this.transform.position = m_Target.position;
+    }
+    
+
+}
---
+++ IT Project/Assets/Scripts/Player/TapToMove.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapToMove : MonoBehaviour {
+
+
+    private NavMeshAgent navMeshAgent;
+
+
+	// Use this for initialization
+	void Start () {
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Fire1"))
+        {
+
+            Move();
+
+
+        }
+
+	
+	}
+
+
+
+    void Move()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            navMeshAgent.destination = hit.point;
+            navMeshAgent.Resume();
+
+        }
+    }
+
+
+
+
+}
+
+
+
+
