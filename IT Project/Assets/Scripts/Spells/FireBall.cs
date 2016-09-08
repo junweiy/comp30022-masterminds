4fa5364028e170458cb1735b60b49da05040c746
Junwei Yang <junweiy@student.unimelb.edu.au>
2016-09-08 16:51:18 +1000

    Fixed fireball position

--- IT Project/Assets/Scripts/Spells/FireBall.cs
+++ IT Project/Assets/Scripts/Spells/FireBall.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class FireBall : Spell {
 	public int damage = 40;
-	public FireBall() : base(10,"",true,10,10,"fire ball","fire ball",10) {
+	public FireBall() : base(10,"",false,10,10,"fire ball","fire ball",10) {
 	}
 
 	public override void applyEffect(Character character,Transform characterTransform,Vector3 destination) {
 		Object fireBallPrefab = Resources.Load("Prefabs/FireBall");
-		Vector3 pos = characterTransform.position + (destination - characterTransform.position);
+		Vector3 pos = characterTransform.position + (destination - characterTransform.position) / 2;
 		Vector3 dir = destination - characterTransform.position;
-		GameObject.Instantiate (fireBallPrefab, pos, Quaternion.LookRotation(dir, Vector3.up));
+		GameObject.Instantiate (fireBallPrefab, pos, Quaternion.LookRotation(dir));
 	}
 
 	public override void levelUp () {
