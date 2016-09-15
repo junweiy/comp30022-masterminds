7af5388352885f9c1ccc667818b964d1a88b19f5
chao li <chaol6@student.unimelb.edu.au>
2016-09-15 14:25:02 +1000

    register and login page script added

---
+++ IT Project/Assets/RegisterPanelController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RegisterPanelController : MonoBehaviour {
+
+    public Text userName_input;
+    public Text email_input;
+    public string userName;
+    public string email;
+
+	
+	// Update is called once per frame
+	void Update () {
+        userName = userName_input.text.ToString();
+        email = email_input.text.ToString();
+	}
+}
