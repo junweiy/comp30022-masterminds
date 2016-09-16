7bd8271a92650a91a61da4304df2b8512535cb36
chao li <chaol6@student.unimelb.edu.au>
2016-09-16 13:13:38 +1000

    added alert page, display error message received from server

---
+++ IT Project/Assets/Scripts/Profile/ProfileErrorPageController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ProfileErrorPageController : MonoBehaviour {
+
+    public Text errorMessage;
+
+    public void showAlert(string message)
+    {
+        errorMessage.text = message;
+    }
+
+}
