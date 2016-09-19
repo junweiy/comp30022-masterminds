c5977f3e6c81296b48572b4b717eb2db028141ef
chao li <chaol6@student.unimelb.edu.au>
2016-09-20 03:42:40 +1000

    [Result Page UI] Modified ResultPageController 1. load and display result, numKill,numDeath implemented 2. display of skill needs implementation 3. character class may needs to have userName and ID 4. character list is null?

--- IT Project/Assets/ResultPageController.cs
+++ IT Project/Assets/ResultPageController.cs
@@ -2,22 +2,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.UI;
 
 public class ResultPageController : MonoBehaviour {
 	private bool isWinner;
 	Character[] chars;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject detailPanel;
+    public RectTransform displayDetailPanel;
+    public GameObject result;
 
-	public void returnToRoom() {
+    // Update is called once per frame
+    void Update()
+    {
+        loadData();
+        //display result (win or lose)
+        if (isWinner)
+        {
+            result.GetComponent<Text>().text = "VICTORY";
+        }
+        else
+        {
+            result.GetComponent<Text>().text = "LOSE";
+        }
+       
+
+        // generate a panel to display details of result for each user
+        for (int i = 0; i < chars.Length; i++)
+        {
+            GameObject userDetail = (GameObject)Instantiate(detailPanel);
+            Text[] texts;
+            texts = userDetail.GetComponentsInChildren<Text>();
+            // character should know user name and id
+            //texts[0].text = chars[i].userName
+            //texts[1].text = chars[i].userID;
+            texts[2].text = chars[i].numKill.ToString();
+            texts[3].text = chars[i].numDeath.ToString();
+            //TODO: load and display spell image needs implementation
+            //...
+            userDetail.transform.SetParent(displayDetailPanel, false);
+            userDetail.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+
+    }
+
+    public void returnToRoom() {
 		StateController.switchToRoom ();
 	}
 
@@ -25,7 +55,7 @@ public class ResultPageController : MonoBehaviour {
 		Character current = GlobalState.instance.currentChar;
 		this.chars = GlobalState.instance.gameController.characters;
 		int maxScore = chars.Max (c => c.score);
-		Character[] winners = chars.Where (c => c.score == maxScore);
+		Character[] winners = chars.Where (c => c.score == maxScore).ToArray();
 		this.isWinner = winners.Contains (current);
 	}
 
