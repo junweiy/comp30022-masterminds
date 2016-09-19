951e28438bbf7571787179c8783e797a8832cef4
chao li <chaol6@student.unimelb.edu.au>
2016-09-20 01:15:54 +1000

    rebuild spell bar script

--- IT Project/Assets/Scripts/Spells/SpellButtonController.cs
+++ IT Project/Assets/Scripts/Spells/SpellButtonController.cs
@@ -4,15 +4,15 @@ using UnityEngine.UI;
 
 public class SpellButtonController : MonoBehaviour {
 
-    public GameObject spellButton;
+    public GameObject spellIcon;
     public Spell spell;
     public Image icon;
 
     void Start()
     {
-        Image[] images;
-        images = spellButton.GetComponentsInChildren<Image>();
-        icon = images[1];
+        //Image[] images;
+        //images = spellButton.GetComponentsInChildren<Image>();
+        //icon = images[1];
     }
 	public void onclick()
     {
@@ -20,5 +20,17 @@ public class SpellButtonController : MonoBehaviour {
             {
                 spell.currentCooldown = 0;
             }
+    }
+
+    void Update()
+    {
+        if (spell.currentCooldown < spell.cooldown)
+        {
+            Image[] images;
+            images = icons[i].GetComponentsInChildren<Image>();
+            Image spellImage = images[1];
+            spells[i].currentCooldown += Time.deltaTime;
+            spellImage.fillAmount = spells[i].currentCooldown / spells[i].cooldown;
+        }
     }
 }
