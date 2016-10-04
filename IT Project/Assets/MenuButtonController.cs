3b50d1cdcde2405f3b11f53cdb46347ed3dcbc12
chao li <chaol6@student.unimelb.edu.au>
2016-10-04 21:31:18 +1100

    Added Menu Button

---
+++ IT Project/Assets/MenuButtonController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MenuButtonController : MonoBehaviour {
+
+    public GameObject menuPanel;
+    public GameObject menuButton;
+    public GameObject pauseButton;
+    public GameObject mainMenuButton;
+
+	public void MenuButtonOnClick()
+    {
+        if (menuPanel.activeInHierarchy)
+        {
+            menuPanel.SetActive(false);
+        }
+        else
+        {
+            menuPanel.SetActive(true);
+        }
+    }
+
+    public void MainMenuButtonOnClick()
+    {
+        StateController.switchToMainMenu();
+    }
+
+    //void Update()
+    //{
+    //    if (menuPanel.activeInHierarchy)
+    //    {
+    //        if (EventSystem.current.gameObject != pauseButton && EventSystem.current.gameObject != mainMenuButton)
+    //        {
+    //            //menuPanel.SetActive(false);
+    //        }
+
+    //    }
+    //}
+}
