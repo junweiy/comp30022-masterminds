8492bff345c51f0daa825b8c87384de1dbe1ddad
jack9966qk <jack9966qk@gmail.com>
2016-10-09 21:57:39 +1100

    Added data strcuture for save file

---
+++ IT Project/Assets/Scripts/Save/GameSave.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameSave {
+    public List<Record> Records;
+    public SaveInfo Info;
+
+    public GameSave(IEnumerable<Record> records, SaveInfo info) {
+        this.Records = new List<Record>(records);
+        this.Info = info;
+    }
+}
