6bd57056f3605c57dccca90332fb6f92f2f08c5b
jack9966qk <jack9966qk@gmail.com>
2016-10-09 22:38:35 +1100

    added SaveInfo which was missing in the last commit

--- IT Project/Assets/Scripts/Save/SaveInfo.cs
+++ IT Project/Assets/Scripts/Save/SaveInfo.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class SaveInfo {
-    
+    public List<string> Usernames;
+    public string Name;
+    public DateTime Time;
 }
