f5b4014521caf81edcc71a081176deadc74f6fc6
Weixu Chen <kevinchen@Kevins-MacBook-Pro-2.local>
2016-10-13 16:12:38 +1100

    -- fixed test for profile and character

--- IT Project/Assets/Editor/CharacterTest.cs
+++ IT Project/Assets/Editor/CharacterTest.cs
@@ -5,19 +5,22 @@ using System;
 
 public class CharacterTest {
 
-    Character character;
+	GameObject characterObject;
+	Character character;
 
 	public const int DAMAGE_TAKEN_FOR_TESTING = 10;
 
     [SetUp]
     public void SetUp() {
-		character = new Character();
+		characterObject = UnityEngine.Object.Instantiate(Resources.Load("Prefabs/Character", typeof(GameObject))) as GameObject;
+		character = characterObject.GetComponent<Character> ();
     }
 
     
 
     [Test]
     public void TakeDamageTest() {
+
 		float currHP = character.hp;
 		character.TakeDamage (DAMAGE_TAKEN_FOR_TESTING);
 		Assert.AreEqual (character.hp, currHP - DAMAGE_TAKEN_FOR_TESTING);
@@ -28,6 +31,7 @@ public class CharacterTest {
 		// if character is died, its hp should be zero and can't be less than 0.
 		Assert.True (character.isDead);
 		Assert.True (character.hp == 0);
+
     }
 
 
---
+++ IT Project/Assets/Editor/ProfileMessengerTest.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System;
+
+public class ProfileMessengerTest {
+
+	private string DummyUsername;
+	private string DummmyEmail;
+
+	[SetUp]
+	public void SetUp() {
+		
+	}
+
+
+	[Test]
+	public void CreateAndGetTest() {
+
+		//first test null input
+		//Assert.AreEqual (ProfileMessenger.createNewUser("",""),null);
+
+		//create a user and get it and see if they have same user id
+		int pid = (int)ProfileMessenger.createNewUser("UnitTest1","unit1@test.com");
+		Profile p = ProfileMessenger.GetProfileByEmail("unit1@test.com");
+		Assert.AreEqual (p.uid, pid);
+
+
+
+	}
+
+
+	[Test]
+	public void SubmitProfileTest() {
+
+		//
+
+	}
+
+
+}
